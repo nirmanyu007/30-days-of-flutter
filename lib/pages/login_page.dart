85af940ee386aade055ce7fa408098a4bd880233
nirmanyu007 <nirmanyuthakur16@gmail.com>
2023-01-14 23:06:59 +0530

    day 4

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,13 +4,51 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Center(
-          child: Text("LoginPage",
-          style: TextStyle(
-            fontSize: 20,
-            color: Color.fromARGB(76, 0, 0, 255),
-            fontWeight: FontWeight.bold,
-          ),),
-    ));
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/hi.png",
+            fit: BoxFit.cover,
+          ),
+          // ignore: prefer_const_constructors
+          SizedBox(
+            height: 20.0,
+          ),
+          // ignore: prefer_const_constructors
+          Text(
+            "welcome",
+            // ignore: prefer_const_constructors
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter Username", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter Passward", labelText: "Passward"),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        print("Hi mister how do you do");
+                      },
+                      child: Text("Login")),
+                ],
+              )),
+        ],
+      ),
+    );
   }
 }
